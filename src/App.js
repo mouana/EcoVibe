ac3128ea972c19adcaaf4cb0c26ff1cc387b142d
mouana <souabni2001mona@gmail.com>
2024-11-20 22:14:01 +0100

    add first home section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Home from './view/Home'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes> 
-      <Route path="/" element={
-          <>
-            
-          </>
-        } />
-       
+      <Route path="/" element={<Home></Home>} />
       </Routes>
       </BrowserRouter>
     
---
+++ src/view/Home.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/headers/Header';
+import SectionOne from '../components/home/SectionOne'
+export default function Home (){
+    return (
+        <div>
+            <Header />
+            <SectionOne />
+        </div>
+    )
+}
